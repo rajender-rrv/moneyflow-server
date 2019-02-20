aa0fdf10fb4e3d33457441f02623068f85b151ac
rreddy <rajender.reddy@impressico.com>
2019-02-20 16:52:59 +0530

    initial project setup

---
+++ src/main/java/com/mf/MoneyflowServerApplication.java
@@ -0,0 +1,14 @@
+package com.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyflowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyflowServerApplication.class, args);
+		System.out.println("server started...");
+	}
+
+}
